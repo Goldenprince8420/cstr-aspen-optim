0f7d7a2d2f12f26fe52cf37834bf999ffcc9ac5d
Rahul Golder <rahulgolder0202@gmail.com>
2024-01-18 12:53:37 +0530

    Added CSTR data code

--- git_push.sh
+++ git_push.sh
@@ -3,5 +3,5 @@ git add .
 git commit -m "Added CSTR data code"
 git branch -M main
 git remote add origin https://github.com/Goldenprince8420/cstr-aspen-optim.git
-git push -u origin main
+git push -u origin main --force
 echo "Done!!"
